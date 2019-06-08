ba96ca599396a804f92f20c0f715aa7231e09c73
jmjatlanta <jmjatlanta@gmail.com>
2019-06-08 10:49:48 -0500

    Impelemented secondary index

--- htlc.cpp
+++ htlc.cpp
@@ -1,5 +1,4 @@
 #include <htlc.hpp>
-#include <eosio/crypto.hpp>
 #include <eosio/system.hpp>
 
 uint64_t htlc::create(eosio::name sender, eosio::name receiver, eosio::asset token, 
@@ -8,15 +7,19 @@ uint64_t htlc::create(eosio::name sender, eosio::name receiver, eosio::asset tok
    require_auth(sender);
    htlc_index htlcs(get_self(), get_first_receiver().value);
    htlc_contract htlc(sender, receiver, token, hashlock, timelock);
+   // make sure an htlc with this hash does not already exist
+   std::shared_ptr<htlc::htlc_contract> old_contract = get_by_id(htlc.id);
+   eosio::check(old_contract == nullptr, "Another HTLC generates that hash. Try changing parameters slightly.");
    // make sure the receiver exists
    assert( is_account( receiver ) );
    // make sure the sender has the funds
    const auto bal = eosio::token::get_balance("eosio.token"_n, sender, token.symbol.code());
    eosio::check(token.amount < bal.amount, "Insuffiient Funds");
    // build the record
-   uint64_t id = htlcs.available_primary_key();
+   uint64_t key = htlcs.available_primary_key();
    htlcs.emplace(sender, [&](auto& row) {
-      row.id = id;
+      row.key = key;
+      row.id = htlc.id;
       row.sender = htlc.sender;
       row.receiver = htlc.receiver;
       row.token = htlc.token;
@@ -30,41 +33,50 @@ uint64_t htlc::create(eosio::name sender, eosio::name receiver, eosio::asset tok
    eosio::action( eosio::permission_level{sender, "active"_n},
          "eosio.token"_n, "transfer"_n,
          std::make_tuple(_self, "eosio.token"_n, token, std::string("Held in HTLC")));
-   return id;
+   return key;
 }
 
-void htlc::withdraw(uint64_t key, std::string preimage)
+void htlc::withdraw(eosio::checksum256 id, std::string preimage)
 {
-   htlc_index htlcs(get_self(), get_first_receiver().value);
-   auto iterator = htlcs.find(key);
+   std::shared_ptr<htlc_contract> contract = get_by_id(id);
    // basic checks
-   eosio::check( iterator != htlcs.end(), "HTLC not found");
-   eosio::check( !(*iterator).withdrawn, "Tokens from this HTLC have already been withdrawn" );
-   eosio::check( !(*iterator).refunded, "Tokens from this HTLC have already been refunded");
-   eosio::check( (*iterator).timelock < eosio::current_block_time(), "HTLC timelock expired");
+   eosio::check( contract != nullptr, "HTLC not found");
+   eosio::check( !contract->withdrawn, "Tokens from this HTLC have already been withdrawn" );
+   eosio::check( !contract->refunded, "Tokens from this HTLC have already been refunded");
+   eosio::check( contract->timelock < eosio::current_block_time(), "HTLC timelock expired");
    eosio::checksum256 passed_in_hash = eosio::sha256(preimage.c_str(), preimage.length());
-   eosio::check( (*iterator).hashlock == passed_in_hash, "Preimage mismatch");
+   eosio::check( contract->hashlock == passed_in_hash, "Preimage mismatch");
    // all looks good, do the transfer
    eosio::action( eosio::permission_level{_self, "active"_n},
          "eosio.token"_n, "transfer"_n,
-         std::make_tuple((*iterator).receiver, "eosio.token"_n, (*iterator).token, 
+         std::make_tuple(contract->receiver, "eosio.token"_n, contract->token, 
                std::string("Withdrawn from HTLC")));
  }
 
-void htlc::refund(uint64_t key)
+void htlc::refund(eosio::checksum256 id)
 {
-   htlc_index htlcs(get_self(), get_first_receiver().value);
-   auto iterator = htlcs.find(key);
+   std::shared_ptr<htlc::htlc_contract> contract = get_by_id(id);
    // basic checks
-   eosio::check( iterator != htlcs.end(), "HTLC not found");
-   eosio::check( !(*iterator).withdrawn, "Tokens from this HTLC have already been withdrawn" );
-   eosio::check( !(*iterator).refunded, "Tokens from this HTLC have already been refunded");
-   eosio::check( (*iterator).timelock >= eosio::current_block_time(), "HTLC timelock expired");
-   // TODO: all looks good, do the refund
+   eosio::check( contract != nullptr, "HTLC not found");
+   eosio::check( !contract->withdrawn, "Tokens from this HTLC have already been withdrawn" );
+   eosio::check( !contract->refunded, "Tokens from this HTLC have already been refunded");
+   eosio::check( contract->timelock >= eosio::current_block_time(), "HTLC timelock has not expired");
+   // all looks good, do the refund
    eosio::action( eosio::permission_level{_self, "active"_n},
          "eosio.token"_n, "transfer"_n,
-         std::make_tuple((*iterator).sender, "eosio.token"_n, (*iterator).token, 
+         std::make_tuple(contract->sender, "eosio.token"_n, contract->token, 
                std::string("Refunded from HTLC")));
+}
+
+std::shared_ptr<htlc::htlc_contract> htlc::get_by_id(eosio::checksum256 id)
+{
+   htlc_index htlcs(get_self(), get_first_receiver().value);
+   auto id_index = htlcs.get_index<"id"_n>();
+   auto iterator = id_index.find(id);
+   if ( iterator == id_index.end() )
+      return nullptr;
+   // make a copy to keep it around
+   return std::shared_ptr<htlc::htlc_contract>( new htlc_contract(*iterator));
 }
 
 
--- htlc.hpp
+++ htlc.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <eosio/crypto.hpp>
 #include <eosio.token/eosio.token.hpp>
 
 class [[eosio::contract]] htlc : public eosio::contract
@@ -23,13 +24,13 @@ class [[eosio::contract]] htlc : public eosio::contract
        * I have the preimage. Send the tokens to the receiver
        */
       [[eosio::action]]
-      void withdraw(uint64_t id, std::string preimage);
+      void withdraw(eosio::checksum256 id, std::string preimage);
 
       /*****
        * Return the tokens to the sender
        */
       [[eosio::action]]
-      void refund(uint64_t id);
+      void refund(eosio::checksum256 id);
 
    private:
 
@@ -38,7 +39,8 @@ class [[eosio::contract]] htlc : public eosio::contract
        */
       struct [[eosio::table]] htlc_contract
       {
-         uint64_t id; // unique key to prevent duplicates
+         uint64_t key; // unique key
+         eosio::checksum256 id;
          eosio::name sender; // who created the HTLC
          eosio::name receiver; // the destination for the tokens
          eosio::asset token; // the token and quantity
@@ -48,7 +50,8 @@ class [[eosio::contract]] htlc : public eosio::contract
          bool refunded; // true if sender is refunded
          std::string preimage; /// the preimage provided by the receiver to claim
 
-         uint64_t primary_key() const { return id; }
+         uint64_t primary_key() const { return key; }
+         eosio::checksum256 by_id() const { return id; }
 
          htlc_contract() {}
          htlc_contract(eosio::name sender, eosio::name receiver, eosio::asset token,
@@ -62,12 +65,33 @@ class [[eosio::contract]] htlc : public eosio::contract
             this->preimage = "";
             this->withdrawn = false;
             this->refunded = false;
-            //TODO: Make a unique secondary index with id to prevent duplicates
+            this->id = create_id(this); // unique secondary index
+            // NOTE: this->key is set elsewhere
          }
-
-      private:
-         
       };
 
-      typedef eosio::multi_index<"htlcs"_n, htlc_contract> htlc_index;
+      /*****
+       * Indexing
+       */
+      typedef eosio::multi_index<"htlcs"_n, htlc_contract,
+            eosio::indexed_by<"id"_n, 
+            eosio::const_mem_fun<htlc_contract, eosio::checksum256, 
+            &htlc_contract::by_id>>> htlc_index;
+
+      /****
+       * Assists in building the id. This should only be called by the ctor, as some fields
+       * must be set to the default for hashing to work correctly
+       * 
+       * @param in the contract to generate the id from
+       * @param the sha256 hash of the contract
+       */
+      static eosio::checksum256 create_id( htlc_contract* const in)
+      {
+         return eosio::sha256( reinterpret_cast<char *>(in), sizeof(htlc_contract)  );
+      }
+
+      /***
+       * Get a contract by its id
+       */
+      std::shared_ptr<htlc::htlc_contract> get_by_id(eosio::checksum256 id); 
 };
