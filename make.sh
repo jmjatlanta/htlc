bb282f607424d4062ab855fe6c4ef7a990f705b7
jmjatlanta <jmjatlanta@gmail.com>
2019-06-28 15:47:54 -0500

    update make script

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-eosio-cpp -abigen -o eos_htlc.wasm eos_htlc.cpp -I../eosio.contracts/contracts/eosio.token/include -I. -I../../../boost169/include
+eosio-cpp -abigen -o htlc.wasm htlc.cpp -I../eosio.contracts/contracts/eosio.token/include -I. -I../../../boost169/include
